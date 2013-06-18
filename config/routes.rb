496ca8e217015a94a951aefb95dfd2938d330551
Dan Childs <danchildsshop@gmail.com>
2013-06-18 21:38:00 +0100

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Hartl::Application.routes.draw do
   get "static_pages/home"
+  get "static_pages/about"
 
   get "static_pages/help"
 
   resources :users
   resources :microposts
+  
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
